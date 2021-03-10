38eb071de2eed569cb623f298fa2f267217e6b7a
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-03-09 20:13:47 -0500

    Data Cleaned

--- api_keys.py
+++ api_keys.py
@@ -1,38 +1,10 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# OpenWeatherMap API Key\n",
-    "weather_api_key = \"999fb85742e96c699338def4a2ba44db\"\n",
-    "\n",
-    "# Priceline\n",
-    "p_key = \"7d7c570015msh06f88cde872d5e5p15c904jsn38364700fb06\"\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+# OpenWeatherMap API Key
+weather_api_key = "999fb85742e96c699338def4a2ba44db"
+
+# Google API Key
+g_key = "AIzaSyAqPPpmTscVbUF0Sro9_BVBTJzdF6xeX1s"
+    
+#hotel.com API
+h_key = "7d7c570015msh06f88cde872d5e5p15c904jsn38364700fb06"
+
+
