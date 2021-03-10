53e61d3f5080768f28aed295bb4a5573bd10dcf3
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-03-09 19:29:39 -0500

    Kayla API Improved

--- api_keys.py
+++ api_keys.py
@@ -1,3 +1,17 @@
+# OpenWeatherMap API Key
+weather_api_key = "999fb85742e96c699338def4a2ba44db"
+
+# Google API Key
+h_key = "7d7c570015msh06f88cde872d5e5p15c904jsn38364700fb06"
+
+
+
+
+
+
+
+
+
 {
  "cells": [
   {
