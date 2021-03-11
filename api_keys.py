2bb2a6819ca660030b0402b6c3a89ec9ae6fc22b
Kayla Kelly <76846236+Kaylawkelly@users.noreply.github.com>
2021-03-10 19:48:00 -0500

    addition of Ryan API

--- api_keys.py
+++ api_keys.py
@@ -7,4 +7,6 @@ g_key = "AIzaSyAqPPpmTscVbUF0Sro9_BVBTJzdF6xeX1s"
 #hotel.com API
 h_key = "7d7c570015msh06f88cde872d5e5p15c904jsn38364700fb06"
 
+#flight API
+f_key = "e5ee88badfmsh65ec7588e95e6fcp181b1cjsn38212393a000"
 
